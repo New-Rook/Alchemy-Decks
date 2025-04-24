5c022a7fa65def88fc247c667150f5e1164b8460
New-Rook <newrookone@gmail.com>
2025-04-24 21:36:29 +0100

    Routing fix

--- src/components/GuestRoute.tsx
+++ src/components/GuestRoute.tsx
@@ -1,12 +1,13 @@
 import { PropsWithChildren, useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { Navigate } from "react-router"
+import { LoadingWheel } from "./LoadingWheel"
 
 export const GuestRoute = ({ children }: PropsWithChildren) => {
     const { authStatus } = useContext(AuthContext)
 
     if (authStatus === 'pending') {
-        return <div />
+        return <LoadingWheel />
     }
 
     return authStatus === 'guest' ? children : <Navigate to={'/'} />
--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren, useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { Navigate } from "react-router"
+import { LoadingWheel } from "./LoadingWheel"
 
 export const PrivateRoute = ({ children }: PropsWithChildren) => {
     const { authStatus } = useContext(AuthContext)
+
+    if (authStatus === 'pending') {
+        return <LoadingWheel />
+    }
 
     return authStatus === 'authenticated' ? children : <Navigate to={'/'} />
 }
